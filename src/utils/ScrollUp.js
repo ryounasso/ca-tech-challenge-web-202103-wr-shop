1dc8bdeadfec292b1ee057d6b38499de6bd76bc8
ryonasso <ryounaoyuki@gmail.com>
2021-03-27 17:05:29 +0900

    add file

---
+++ src/utils/ScrollUp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ScrollToTop from 'react-scroll-to-top'
+
+function ScrollUp() {
+
+    return (
+        <ScrollToTop
+         smooth
+         style={{
+             borderRadius: '50%'
+         }}
+         />
+        );
+}
+
+export default ScrollUp;
